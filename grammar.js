268bce9784d40107635a37c8b32895d3d9d51206
Ole Herman Schumacher Elgesem <ole.elgesem@northern.tech>
2024-04-15 20:09:11 +0200

    autoformat
    Signed-off-by: Ole Herman Schumacher Elgesem <ole.elgesem@northern.tech>

--- grammar.js
+++ grammar.js
@@ -1,120 +1,100 @@
 module.exports = grammar({
-  name: 'cfengine',
+  name: "cfengine",
 
-  extras: $ => [
-    $.comment,
-    $.macro,
-    /[\s]+/
-  ],
+  extras: ($) => [$.comment, $.macro, /[\s]+/],
 
   rules: {
-    source_file: $ => repeat($._block),
+    source_file: ($) => repeat($._block),
 
-    _block: $ => choice(
-      $.bundle_block
-      // TODO: body
-      // TODO: promise
-    ),
-
-    bundle_block: $ => seq(
-      $.bundle_keyword,
-      alias($.identifier, $.bundle_type),
-      alias($.identifier, $.bundle_name),
-      optional($.parameter_list),
-      $.bundle_body
-    ),
-    bundle_keyword: _ => 'bundle',
-
-    parameter_list: $ => seq(
-      '(',
-      optional(
-        seq(
-          repeat(
-            seq(
-              alias($.identifier, $.parameter), ','
-            )
-          ),
-          alias($.identifier, $.parameter),
-          optional(',')
-        )
+    _block: ($) =>
+      choice(
+        $.bundle_block,
+        // TODO: body
+        // TODO: promise
       ),
-      ')'
-    ),
 
-    bundle_body: $ => seq(
-      '{',
-      repeat($.bundle_section),
-      '}'
-    ),
+    bundle_block: ($) =>
+      seq(
+        $.bundle_keyword,
+        alias($.identifier, $.bundle_type),
+        alias($.identifier, $.bundle_name),
+        optional($.parameter_list),
+        $.bundle_body,
+      ),
+    bundle_keyword: (_) => "bundle",
 
-    bundle_section: $ => seq(
-      $.promise_guard,
-      // 0 or more promises without a class guard:
-      repeat($.promise),
-      // 0 or more class guards with 0 or more promises insde:
-      repeat($.class_guarded_promises)
-    ),
+    parameter_list: ($) =>
+      seq(
+        "(",
+        optional(
+          seq(
+            repeat(seq(alias($.identifier, $.parameter), ",")),
+            alias($.identifier, $.parameter),
+            optional(","),
+          ),
+        ),
+        ")",
+      ),
 
-    _right_value: $ => choice(
-      $.quoted_string,
-      $.qualified_identifier,
-      $.list,
-      $.call,
-    ),
+    bundle_body: ($) => seq("{", repeat($.bundle_section), "}"),
 
-    call: $ => seq(
-      alias($.qualified_identifier, $.calling_identifier),
-      '(',
-      optional($._value_list),
-      ')'
-    ),
+    bundle_section: ($) =>
+      seq(
+        $.promise_guard,
+        // 0 or more promises without a class guard:
+        repeat($.promise),
+        // 0 or more class guards with 0 or more promises insde:
+        repeat($.class_guarded_promises),
+      ),
 
-    list: $ => seq(
-      '{',
-      optional($._value_list),
-      '}'
-    ),
+    _right_value: ($) =>
+      choice($.quoted_string, $.qualified_identifier, $.list, $.call),
+
+    call: ($) =>
+      seq(
+        alias($.qualified_identifier, $.calling_identifier),
+        "(",
+        optional($._value_list),
+        ")",
+      ),
+
+    list: ($) => seq("{", optional($._value_list), "}"),
 
     // inner part of a non-empty list of values
     // such as the arguments of a function call
     // or the strings in an slist
-    _value_list: $ => seq(
-      $._right_value,
-      repeat(seq(',', $._right_value)),
-      optional(',')
-    ),
+    _value_list: ($) =>
+      seq($._right_value, repeat(seq(",", $._right_value)), optional(",")),
 
-    class_guarded_promises: $ => prec.right(1, seq(
-      $.class_guard,
-      prec(1, repeat($.promise))
-    )),
+    class_guarded_promises: ($) =>
+      prec.right(1, seq($.class_guard, prec(1, repeat($.promise)))),
 
-    promise: $ => seq(
-      alias($.quoted_string, $.promiser),
-      optional(seq('->', alias($._right_value, $.stakeholder))),
-      optional($.attribute),
-      repeat(seq(',', $.attribute)),
-      optional(','),
-      ';'
-    ),
+    promise: ($) =>
+      seq(
+        alias($.quoted_string, $.promiser),
+        optional(seq("->", alias($._right_value, $.stakeholder))),
+        optional($.attribute),
+        repeat(seq(",", $.attribute)),
+        optional(","),
+        ";",
+      ),
 
-    attribute: $ => seq(
-      alias($.identifier, $.attribute_name),
-      '=>',
-      $._right_value
-    ),
+    attribute: ($) =>
+      seq(alias($.identifier, $.attribute_name), "=>", $._right_value),
 
-    quoted_string: $ => /\"((\\(.|\n))|[^"\\])*\"|\'((\\(.|\n))|[^'\\])*\'|`[^`]*`/,
+    quoted_string: ($) =>
+      /\"((\\(.|\n))|[^"\\])*\"|\'((\\(.|\n))|[^'\\])*\'|`[^`]*`/,
 
-    identifier: $ => /[a-zA-Z0-9_]+/,
-    qualified_identifier: $ => /([a-zA-Z0-9_]+\:)?([a-zA-Z0-9_]+\.)?[a-zA-Z0-9_]+/,
+    identifier: ($) => /[a-zA-Z0-9_]+/,
+    qualified_identifier: ($) =>
+      /([a-zA-Z0-9_]+\:)?([a-zA-Z0-9_]+\.)?[a-zA-Z0-9_]+/,
 
-    promise_guard: $ => /[a-zA-Z_]+:/,
+    promise_guard: ($) => /[a-zA-Z_]+:/,
 
-    class_guard: $ => /[.|&!()a-zA-Z0-9_:][\t .|&!()a-zA-Z0-9_:]*::/,
+    class_guard: ($) => /[.|&!()a-zA-Z0-9_:][\t .|&!()a-zA-Z0-9_:]*::/,
 
-    comment: $ => token(seq('#', /.*/)),
+    comment: ($) => token(seq("#", /.*/)),
 
-    macro: $ => token(/@[^\n].*/)
-  }
-})
+    macro: ($) => token(/@[^\n].*/),
+  },
+});
