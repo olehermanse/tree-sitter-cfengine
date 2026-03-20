660c0559633240da46b74a7690c6738351e96aeb
Ole Herman Schumacher Elgesem <ole@northern.tech>
2026-03-20 18:29:48 +0100

    Bump to version 1.1.6
    Signed-off-by: Ole Herman Schumacher Elgesem <ole@northern.tech>

--- src/parser.c
+++ src/parser.c
@@ -3137,7 +3137,7 @@ TS_PUBLIC const TSLanguage *tree_sitter_cfengine(void) {
     .metadata = {
       .major_version = 1,
       .minor_version = 1,
-      .patch_version = 5,
+      .patch_version = 6,
     },
   };
   return &language;
